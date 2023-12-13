21934410bcc8764b22e3f2170d4175afda882ef2
atthana phiphatthanaloetmongkhon <atthana.p@codium.co>
2023-12-13 18:01:33 +0700

    Practice "event binding" and "property binding".

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+  title = 'About Q';
+  logo = './assets/images/logo-jennie.png';
+  logoWidth = 400;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  go(){
+    // alert('Hello GRASSROOT Q');
+    this.title = 'LOVE YOU'
   }
 
 }
