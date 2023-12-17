b272fc3a8019bd653b8b5cd7251bb3ce27d0f7e5
atthana phiphatthanaloetmongkhon <atthana.p@codium.co>
2023-12-18 02:03:12 +0700

    ngIf, ngFor, ngClass studying.

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -9,6 +9,15 @@ export class AboutComponent implements OnInit {
   title = 'About Q';
   logo = './assets/images/logo-jennie.png';
   logoWidth = 400;
+  isShow = true;
+  users = ['John', 'Mary', 'Bob'];
+  courses = [
+    {'name': 'PHP', 'price': 100},
+    {'name': 'Angular', 'price': 150},
+    {'name': 'ReactP', 'price': 200},
+  ];
+  myColor = 'yellow';
+  isActive = false;
 
   constructor() { }
 
@@ -17,7 +26,9 @@ export class AboutComponent implements OnInit {
 
   go(){
     // alert('Hello GRASSROOT Q');
-    this.title = 'LOVE YOU'
+    this.title = 'LOVE YOU';
+    this.myColor = 'green';
+    this.isActive = true;
   }
 
 }
