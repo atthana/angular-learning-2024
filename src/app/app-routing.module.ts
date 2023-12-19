f608cd682edb42814dea9e997efc07c2a6a1a66b
atthana phiphatthanaloetmongkhon <atthana.p@codium.co>
2023-12-20 01:22:02 +0700

    Created tab "Products" in header.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { ProductsComponent } from './products/products.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
-  { path: 'about', component: AboutComponent}
+  { path: 'about', component: AboutComponent},
+  { path: 'products', component: ProductsComponent}
 ];
 
 @NgModule({
