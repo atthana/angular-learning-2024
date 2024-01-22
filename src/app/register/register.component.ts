8eea0283db0d0e11c622c8224f31d7824c743cad
atthana phiphatthanaloetmongkhon <atthana.p@codium.co>
2024-01-22 22:22:08 +0700

    Remove unused codes.

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../shared/auth.service';
-import { addListener } from 'process';
 import { Router } from '@angular/router';
 
 @Component({
